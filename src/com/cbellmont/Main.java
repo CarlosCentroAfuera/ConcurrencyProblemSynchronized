b171eb2a587e793743f89f2ea5409a92e53cd094
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-10-13 13:06:58 +0200

    joins ordered

--- src/com/cbellmont/Main.java
+++ src/com/cbellmont/Main.java
@@ -6,16 +6,15 @@ public class Main {
         try {
             ThreadWithdrawMoney t1 = new ThreadWithdrawMoney(25);
             t1.start();
+            t1.join();
             ThreadWithdrawMoney t2 = new ThreadWithdrawMoney(25);
             t2.start();
+            t2.join();
             ThreadWithdrawMoney t3 = new ThreadWithdrawMoney(25);
             t3.start();
+            t3.join();
             ThreadWithdrawMoney t4 = new ThreadWithdrawMoney(25);
             t4.start();
-            t1.join();
-            t2.join();
-            t3.join();
-
             t4.join();
 
         } catch (InterruptedException e) {
